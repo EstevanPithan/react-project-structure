04f44e94f2ac76f3177c114be13fbc2695108270
Estevan <estevanpithan@hotmail.com>
2024-03-15 11:29:26 -0300

    feat: creating and setting configuration files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,37 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import { dependencies } from './package.json';
 
-// https://vitejs.dev/config/
+function renderChunks(deps: Record<string, string>) {
+  let chunks = {};
+  Object.keys(deps).forEach(key => {
+    if (['react', 'react-router-dom', 'react-dom'].includes(key)) return;
+    chunks[key] = [key];
+  });
+  return chunks;
+}
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@shared': resolve(__dirname, 'shared')
+    }
+  },
+  build: {
+    outDir: 'build',
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      },
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-router-dom', 'react-dom'],
+          ...renderChunks(dependencies)
+        }
+      }
+    }
+  }
+});
